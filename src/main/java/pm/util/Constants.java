684d25670694fd8d806a86b365a9f6bf537d1500
SoftDryzz <xtoftome@gmail.com>
2026-02-17 19:40:41 +0100

    chore: bump version to 1.1.0 for release

--- src/main/java/pm/util/Constants.java
+++ src/main/java/pm/util/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
      * Current version of ProjectManager.
      * Follows Semantic Versioning (MAJOR.MINOR.PATCH).
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     // ============================================================
     // SYSTEM DIRECTORIES AND FILES
